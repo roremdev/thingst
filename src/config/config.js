216b14b847287c04da2c54f6efd683f825c8c97a
roremDev <rorem.94.developer@gmail.com>
2021-10-15 15:39:01 -0500

    feat: ✨ orm integration

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,21 @@
 if (process.env.NODE_ENV !== 'production')
     require('dotenv').config({ path: './src/config/.env' });
 
-export default {
+/**
+ * @description native variables of server
+ */
+export const server = {
     port: process.env.PORT,
     mode: process.env.MODE || process.env.NODE_ENV,
+};
+
+/**
+ * @description variables of database server
+ */
+export const database = {
+    dbName: process.env.PGDATABASE,
+    dbHost: process.env.PGHOST || 'localhost',
+    dbPort: process.env.PGPORT || '5432',
+    dbUser: process.env.PGUSER,
+    dbPassword: process.env.PGPASSWORD,
 };
---
+++ src/libs/Sequelize.js
@@ -0,0 +1,44 @@
+import { Sequelize as SequelizeClient } from 'sequelize';
+import { database } from '../config/config';
+import Error from '../utils/Error';
+
+const USER = encodeURIComponent(database.dbUser);
+const PASSWORD = encodeURIComponent(database.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${database.dbHost}:${database.dbPort}/${database.dbName}`;
+
+export default class Sequelize {
+    /**
+     * @private
+     * @description singleton pattern for pool connection
+     * @returns {object} - connection client
+     */
+    async #connect() {
+        try {
+            if (!Sequelize.connection) {
+                Sequelize.connection = new SequelizeClient(URI, {
+                    logging: false,
+                });
+                await Sequelize.connection.authenticate();
+            }
+            return Sequelize.connection;
+        } catch ({ message }) {
+            throw new Error(message, 'DRIVER');
+        }
+    }
+    /**
+     * @description process definition for create database tables
+     * @param {string} name - table name
+     * @param {string} schema - table description
+     * @returns {Promise} - response of library
+     */
+    async define(name, schema) {
+        try {
+            const db = await this.#connect();
+            const model = await db.define(name, schema);
+            return await model.sync();
+        } catch (error) {
+            if (!error) throw new Error(message, 'DEFINITION');
+            throw error;
+        }
+    }
+}
